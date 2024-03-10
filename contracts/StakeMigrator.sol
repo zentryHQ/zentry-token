368410280cf569c4675f06e66c14a5137aa412a7
jarindr <jarindr23@gmail.com>
2024-03-10 16:00:25 +0700

    pending migrator

---
+++ contracts/StakeMigrator.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract StakeMigrator is Ownable {
+    constructor(address owner) Ownable(owner) {}
+
+    function stakeMigrate() external onlyOwner {
+        //migrate from stake pool to new stake pool
+        // for single stake pool, withdraw all and stake to new pool while keeping all shares relatively to the amount they deposited in the old pool (if we decided to migrate for them)
+    }
+}
