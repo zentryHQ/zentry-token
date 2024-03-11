2aebe1a92472b145b3877790f9d05899c4e9e43a
Supasin Liulak <palsp2406@gmail.com>
2024-03-11 16:13:30 +0700

    add deploy script

---
+++ ignition/modules/ZentryToken.ts
@@ -0,0 +1,21 @@
+import { buildModule } from '@nomicfoundation/hardhat-ignition/modules';
+
+const gfToken = '0xaaef88cea01475125522e117bfe45cf32044e238';
+const deployer = '';
+const owner = '';
+
+export default buildModule('ZentryToken', (m) => {
+  const migrator = m.contract('Migrator', [gfToken, deployer]);
+
+  const zentryToken = m.contract('ZentryToken', [migrator]);
+
+  const setZentryToken = m.call(migrator, 'setZentryToken', [zentryToken], {
+    after: [zentryToken],
+  });
+
+  m.call(migrator, 'transferOwnership', [owner], {
+    after: [setZentryToken],
+  });
+
+  return { migrator, zentryToken };
+});
