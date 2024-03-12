2a578af49f4fb10e2946c191806ca0468ec02be2
jarindr <jarindr23@gmail.com>
2024-03-13 00:40:53 +0700

    fix renounce role

--- ignition/modules/ZentryToken.ts
+++ ignition/modules/ZentryToken.ts
@@ -5,7 +5,7 @@ const gfToken = "0xaaef88cea01475125522e117bfe45cf32044e238";
 const owner = "0x9DE6DE21007312eeb6CD90500fFECafeC297B70D";
 const publicAddressDeployer = "0xAe7Be8c6d00B46CfeC2Fb5744003012612404B9e";
 const MINTER_ROLE = ethers.keccak256(ethers.toUtf8Bytes("MINTER_ROLE"));
-const DEFAULT_ROLE = ethers.keccak256(ethers.toUtf8Bytes("0x"));
+const DEFAULT_ADMIN_ROLE = ethers.ZeroHash
 export default buildModule("ZentryToken", (m) => {
   const zentryToken = m.contract("ZentryToken");
 
@@ -15,7 +15,7 @@ export default buildModule("ZentryToken", (m) => {
   const grantRole = m.call(zentryToken, "grantRole", [MINTER_ROLE, migrator]);
 
   // grant role mintter
-  m.call(zentryToken, "renounceRole", [DEFAULT_ROLE, publicAddressDeployer], { after: [grantRole] });
+  m.call(zentryToken, "renounceRole", [DEFAULT_ADMIN_ROLE, publicAddressDeployer], { after: [grantRole] });
 
   // transfer ownership to multisig
   m.call(migrator, "transferOwnership", [owner]);
