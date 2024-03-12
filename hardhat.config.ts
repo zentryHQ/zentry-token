fd9f37eb6cb34d1446e6b94136ebf798d9db0496
jarindr <jarindr23@gmail.com>
2024-03-13 01:45:17 +0700

    test deploy hardhat token

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,30 +1,34 @@
-import { config as dotEnvConfig } from 'dotenv';
+import { config as dotEnvConfig } from "dotenv";
 dotEnvConfig();
 
 // hardhat.config.js
-import '@nomicfoundation/hardhat-ignition-ethers';
+import "@nomicfoundation/hardhat-ignition-ethers";
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: '0.8.20',
+  solidity: "0.8.20",
   networks: {
     hardhat: {
       forking: {
-        url: process.env.MAINNET_FORK_RPC_URL || '',
+        url: process.env.MAINNET_FORK_RPC_URL || "",
         // blockNumber: process.env.MAINNET_FORK_BLOCK_NUMBER || '',
       },
+      accounts: { privateKey: process.env.ETH_MAINNET_PRIVATE_KEY },
     },
     localhost: {
-      url: 'http://localhost:8545',
+      url: "http://localhost:8545",
     },
     mainnet: {
-      url: process.env.ETH_MAINNET_RPC_URL || '',
+      url: process.env.ETH_MAINNET_RPC_URL || "",
       accounts: [process.env.ETH_MAINNET_PRIVATE_KEY].filter(Boolean),
+      etherscan: {
+        apiKey: process.env.ETHERSCAN_API_KEY,
+      }
     },
     sepolia: {
-      url: process.env.SEPOLIA_RPC_URL || '',
+      url: process.env.SEPOLIA_RPC_URL || "",
       accounts: [process.env.SEPOLIA_PRIVATE_KEY].filter(Boolean),
     },
   },
