4bf13bb8e55e66d0c58507a5524b2c392090388a
Supasin Liulak <palsp2406@gmail.com>
2024-03-19 13:10:10 +0700

    added etherscan plugin

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,6 +9,7 @@ import 'hardhat-deploy'
 import 'hardhat-contract-sizer'
 import '@nomiclabs/hardhat-ethers'
 import '@layerzerolabs/toolbox-hardhat'
+import '@nomicfoundation/hardhat-verify'
 import { HardhatUserConfig, HttpNetworkAccountsUserConfig } from 'hardhat/types'
 
 import { EndpointId } from '@layerzerolabs/lz-definitions'
@@ -73,6 +74,11 @@ const config: HardhatUserConfig = {
     namedAccounts: {
         deployer: {
             default: 0, // wallet address of index[0], of the mnemonic in .env
+        },
+    },
+    etherscan: {
+        apiKey: {
+            sepolia: process.env.SEPOLIA_ETHERSCAN_API_KEY || '',
         },
     },
 }
