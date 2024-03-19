5c23086fda1e40f878a439b2e9a8ea986b64cdec
Supasin Liulak <palsp2406@gmail.com>
2024-03-19 11:36:08 +0700

    update mock token

--- contracts/mocks/ZentryOFTMock.sol
+++ contracts/mocks/ZentryOFTMock.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
-import {ZentryTokenOFT} from "../ZentryTokenOFT.sol";
+import {ZentryOFT} from "../ZentryOFT.sol";
 
 // @dev WARNING: This is for testing purposes only
-contract ZentryTokenMock is ZentryTokenOFT {
-    constructor(address _lzEndpoint, address _delegate) ZentryTokenOFT(_lzEndpoint, _delegate) {}
+contract ZentryTokenMock is ZentryOFT {
+    constructor(address _lzEndpoint, address _delegate) ZentryOFT(_lzEndpoint, _delegate) {}
 
     function mint(address _to, uint256 _amount) public {
         _mint(_to, _amount);
