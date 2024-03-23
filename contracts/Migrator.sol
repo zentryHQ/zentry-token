18013a6acc621e0c3a49a0b1f3d5294bd79c02b6
Supasin Liulak <palsp2406@gmail.com>
2024-03-23 10:45:20 +0700

    fixed solidity version

--- contracts/Migrator.sol
+++ contracts/Migrator.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity 0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "./ZentryToken.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ZentryToken} from "./ZentryToken.sol";
 
 contract Migrator is Ownable {
     IERC20 public immutable gfToken;
