4979aad2a96de0a26b0c54044ff6f5d690208f10
jarindr <jarindr23@gmail.com>
2024-03-23 12:37:09 -0700

    format import

--- contracts/ZentryToken.sol
+++ contracts/ZentryToken.sol
@@ -1,20 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
-import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { ERC20Burnable } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import { AccessControl } from "@openzeppelin/contracts/access/AccessControl.sol";
+import { ERC20Permit } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 /// @custom:security-contact security@zentry.com
 contract ZentryToken is ERC20, ERC20Burnable, AccessControl, ERC20Permit {
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+  bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-    constructor() ERC20("Zentry", "ZENT") ERC20Permit("Zentry") {
-        _grantRole(DEFAULT_ADMIN_ROLE, _msgSender());
-    }
+  constructor() ERC20("Zentry", "ZENT") ERC20Permit("Zentry") {
+    _grantRole(DEFAULT_ADMIN_ROLE, _msgSender());
+  }
 
-    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
-        _mint(to, amount);
-    }
+  function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+    _mint(to, amount);
+  }
 }
