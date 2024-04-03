e106cc0b1706b5dcb732680ae88bc84f08c6e376
Supasin Liulak <palsp2406@gmail.com>
2024-04-03 12:54:18 +0700

    added fork-node script

--- hardhat.config.forking.ts
+++ hardhat.config.forking.ts
@@ -1,15 +1,15 @@
-import { config as dotEnvConfig } from 'dotenv';
+import { config as dotEnvConfig } from "dotenv";
 dotEnvConfig();
 
 // hardhat.config.js
-import '@nomicfoundation/hardhat-ignition-ethers';
+import "@nomicfoundation/hardhat-ignition-ethers";
 import "@nomicfoundation/hardhat-verify";
-import "hardhat-gas-reporter"
+import "hardhat-gas-reporter";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 export default {
   solidity: {
-    version: '0.8.20',
+    version: "0.8.20",
     settings: {
       viaIR: true,
       optimizer: {
@@ -23,18 +23,18 @@ export default {
   },
   gasReporter: {
     enabled: true,
-    L1: 'ethereum',
+    L1: "ethereum",
     L1Etherscan: process.env.ETHERSCAN_API_KEY,
     reportPureAndViewMethods: true,
-    coinmarketcap: 'a6c9f552-88c8-4c29-80c1-bc62b7b0495c',
+    coinmarketcap: "a6c9f552-88c8-4c29-80c1-bc62b7b0495c",
     darkMode: true,
     includeIntrinsicGas: true,
   },
   networks: {
     hardhat: {
-      blockNumber: 19408785,
+      blockNumber: 19566362,
       forking: {
-        url: process.env.MAINNET_FORK_RPC_URL || '',
+        url: process.env.ETH_MAINNET_RPC_URL || "",
       },
     },
   },
