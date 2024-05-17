d70050f6760808e67a10f059acf92110e1eec512
Supasin Liulak <palsp2406@gmail.com>
2024-05-17 18:09:31 +0700

    fix deploy version

--- contracts/ZentryOFT.sol
+++ contracts/ZentryOFT.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.20;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
 
 import {OFT} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/OFT.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
