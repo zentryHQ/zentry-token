3662a1b4a1c553f55c714af9bb2123f2e0137131
Supasin Liulak <palsp2406@gmail.com>
2024-05-17 15:40:46 +0700

    Update send.ts

--- scripts/send.ts
+++ scripts/send.ts
@@ -3,29 +3,34 @@ import { ethers } from 'hardhat'
 import { EndpointId } from '@layerzerolabs/lz-definitions'
 import { Options } from '@layerzerolabs/lz-v2-utilities'
 
-const mumbai = {
-    contractAddress: '0x18dA2b9c46374EE3a2fC4d52D2421A680622aa0d',
-    eid: EndpointId.POLYGON_V2_TESTNET,
-}
-
 const sepolia = {
-    contractAddress: '0x176BF52F7c0B0508f5b71306bd8BA2E98992c0D2',
     eid: EndpointId.SEPOLIA_V2_TESTNET,
+    contractAddress: '0xBF8228aeb591bF86786746D433FecC03EC18bd83',
 }
 
-const ftmTestnet = {
-    contractAddress: '0x5343646AA34E137937A2E530BA3B642bA807e2d4',
-    eid: EndpointId.FANTOM_V2_TESTNET,
+const baseSepolia = {
+    eid: EndpointId.BASESEP_V2_TESTNET,
+    contractAddress: '0xe9F8bd6E887b21F2Ce6d841dd61fa43455733B4e',
 }
 
+const blastTestnet = {
+    eid: EndpointId.BLAST_V2_TESTNET,
+    contractAddress: '0xe9F8bd6E887b21F2Ce6d841dd61fa43455733B4e',
+}
+
+const arbSepolia = {
+    eid: EndpointId.ARBSEP_V2_TESTNET,
+    contractAddress: '0xe9F8bd6E887b21F2Ce6d841dd61fa43455733B4e',
+}
 /******* Adjust Parameters here *************************************************/
-const from = sepolia
-const to = mumbai
-const receiver = ''
-const tokensToSend = ethers.utils.parseEther('10')
+const from = arbSepolia
+const to = sepolia
+const receiver = '0x86d00B700E302Ee2EB687bAdEd855223Db888B58'
+const tokensToSend = ethers.utils.parseEther('100000')
 /****************************************************************************** */
 
 async function main() {
+    console.log(`Sending ${ethers.utils.formatEther(tokensToSend)} tokens `)
     const ZentOFT = await ethers.getContractFactory('ZentryOFT')
 
     const signers = await ethers.getSigners()
@@ -35,14 +40,19 @@ async function main() {
     const oft = ZentOFT.attach(from.contractAddress).connect(owner)
 
     // Defining extra message execution options for the send operation
-    const options = Options.newOptions().addExecutorLzReceiveOption(200000, 0).toHex().toString()
+    const options = Options.newOptions().addExecutorLzReceiveOption(100000, 0).toHex().toString()
     const sendParam = [to.eid, ethers.utils.zeroPad(receiver, 32), tokensToSend, tokensToSend, options, '0x', '0x']
 
     // Fetching the native fee for the token send operation
     const [nativeFee] = await oft.quoteSend(sendParam, false)
 
+    console.log(`Native Fee: ${ethers.utils.formatEther(nativeFee)}`)
+
     // Executing the send operation from myOFTA contract
-    await oft.send(sendParam, [nativeFee, 0], owner.address, { value: nativeFee })
+    const tx = await oft.send(sendParam, [nativeFee, 0], owner.address, { value: nativeFee })
+    const response = await tx.wait(1)
+    console.log('✅ Transaction submitted')
+    console.log(response)
 }
 
 main()
