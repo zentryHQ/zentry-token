8860f9d801bf229ac4a41ade8b658b7838c58e0c
Phureewat A <a.phureewat@gmail.com>
2025-01-07 11:21:12 +0700

    feat: deploy superchain token task

--- tasks/deploySuperchainToken.ts
+++ tasks/deploySuperchainToken.ts
@@ -1,5 +1,6 @@
 import { task } from "hardhat/config"
 import { networks } from "../config"
+import { ethers } from "ethers"
 import OPTIMISM_MINTABLE_ERC20_FACTORY_ABI from "../abi/OptimismMintableERC20Factory.json"
 
 interface DeployL2TokenArgs {
@@ -10,7 +11,7 @@ interface DeployL2TokenArgs {
 task("deployL2Token", "Deploy L2 token")
 	.addParam("name", "The name of the token")
 	.addParam("symbol", "The symbol of the token")
-	.setAction(async ({ name, symbol }: DeployL2TokenArgs, { ethers, network, run }) => {
+	.setAction(async ({ name, symbol }: DeployL2TokenArgs, { ethers, network }) => {
 		const networkName = network.name
 		const networkConfig = networks[networkName as keyof typeof networks]
 
@@ -25,16 +26,16 @@ task("deployL2Token", "Deploy L2 token")
 			networkConfig.remoteZentryToken,
 			name,
 			symbol
-    )
-    console.log(`TX hash: ${tx.hash}`)
-    const txResult = await tx.wait()
+		)
+		console.log(`Transaction hash: ${tx.hash}`)
+		const txReceipt = await tx.wait()
 
-    const iface = new ethers.Interface(OPTIMISM_MINTABLE_ERC20_FACTORY_ABI)
-    const tokenAddress = getTokenAddress(iface, txResult)
-    console.log(`Token deployed at ${tokenAddress} on ${networkName}`)
-})
+		const iface = new ethers.Interface(OPTIMISM_MINTABLE_ERC20_FACTORY_ABI)
+		const tokenAddress = getTokenAddress(iface, txReceipt.logs)
+		console.log(`Token deployed at ${tokenAddress} on ${networkName}`)
+	})
 
-function getTokenAddress(factoryInterface: any, txResult: any) {
-    const log = factoryInterface.parseLog(txResult.logs[1])
-    return log?.args[0]
+function getTokenAddress(factoryInterface: ethers.Interface, txReceipt: ethers.TransactionReceipt) {
+	const log = factoryInterface.parseLog(txReceipt.logs[1])
+	return log?.args[0]
 }
